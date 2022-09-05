ef31c413e20b9071b60bb4a57a92e81acb49f014
ManirathinamSoundirarajan <manirathinam.soundirarajan@sgkinc.com>
2022-09-05 14:14:15 +0530

    stage 01 complete

--- src/load_data.py
+++ src/load_data.py
@@ -14,7 +14,7 @@ def load_and_save(config_path):
     raw_data_path=config["load_data"]["raw_dataset_csv"]
     df.to_csv(raw_data_path, sep=',', index=False, header=new_cols)
  
- 
+
 if __name__=="__main__":
     args=argparse.ArgumentParser() 
     args.add_argument("--config", default="params.yaml")
