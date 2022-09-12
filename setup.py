fc13e94d786a35f08a0a52c18b2975eaa106de66
ManirathinamSoundirarajan <manirathinamsoundirarajan@sgkinc.com>
2022-09-12 13:43:04 +0530

    setup done

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='src', 
+    version='0.0.1', 
+    description='its a wine quality package',
+    author="ManirathinamSoundirarajan",
+    packages=find_packages(),
+    license='MIT',
+    )
